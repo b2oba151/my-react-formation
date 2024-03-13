939d5920f3b7bd248f94f8f42a2763ef9b32641d
b2oba151 <bodiarra20@gmail.com>
2024-03-13 15:39:23 +0000

    compter_le_nombre_de_fois_où_un_bouton_est_cliqué

--- src/App.jsx
+++ src/App.jsx
@@ -101,18 +101,65 @@ import './App.css';
 //   );
 // }
 
+
+
+
+
+
+
+
+
+
 /* repondre aux eenements*/ 
-function handleClick() {
-  alert('Click effectue');
+// function handleClick() {
+//   alert('Click effectue');
+// }
+
+// function App() {
+//   return (
+//     <>
+//     {/* pas de parenthese apres handleClick  */}
+//     <button onClick={handleClick}>
+//       Click me
+//     </button>
+//     </>
+//   );
+// }
+
+
+
+
+/* compter_le_nombre_de_fois_où_un_bouton_est_cliqué  *//*
+  const [a, b] = useState(0);
+
+deux choses pour useState: 
+l'état actuel ( a), 
+et la fonction qui permet de le mettre à jour ( b)
+useState(0) veut dire que l'etat a ete initialiser par 0
+*/
+
+function MyButton() {
+  const [count, setCount] = useState(0);
+
+  function handleClick() {
+    setCount(count + 1);
+  }
+
+  return (
+    <button onClick={handleClick}>
+      Cliquer {count} fois
+    </button>
+  );
 }
+
+
 
 function App() {
   return (
     <>
-    {/* pas de parenthese apres handleClick  */}
-    <button onClick={handleClick}>
-      Click me
-    </button>
+    <MyButton/>
+    {/* chaque bouton aura son propre etat */}
+    <MyButton/>
     </>
   );
 }
