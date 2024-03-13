e3b851e6dd078c6f3766ffb6658644a98f0a8824
b2oba151 <bodiarra20@gmail.com>
2024-03-13 14:33:52 +0000

    condition if jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,60 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useState, useEffect } from 'react';
+import reactLogo from './assets/react.svg';
+import viteLogo from '/vite.svg'; 
+import './App.css';
+
+function Greeting() {
+  const currentHour = new Date().getHours();
+  let greetingMessage;
+
+  if (currentHour >= 6 && currentHour < 12) {
+    greetingMessage = 'Bonjour';
+  } else if (currentHour >= 12 && currentHour < 18) {
+    greetingMessage = 'Bonne après-midi';
+  } else {
+    greetingMessage = 'Bonne soirée';
+  }
+
+  return (
+    <h2>{greetingMessage}, bienvenue sur notre site !</h2>
+  );
+}
+
+function MyContent({ user }) { // Utilisation de la destructuration pour extraire user
+  return (
+    <div>
+      <h1>{user.name}</h1>
+      <img
+        className="avatar"
+        src={user.imageUrl}
+        alt={`Photo de ${user.name}`} // Utilisation de l'interpolation de chaîne
+        style={{
+          width: user.imageSize,
+          height: user.imageSize,
+        }}
+      />
+    </div>
+  );
+}
+
+function MyConditionElementNoOk() {
+  return (
+    <h3>Condition non vérifiée</h3>
+  );
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 function App() {
   const user = {
@@ -10,20 +63,39 @@ function App() {
     imageSize: 100,
   };
 
+  let content;
+
+  if (user.name == 'Hedy Lamarr') {
+    content = <MyContent user={user} />;
+  } else {
+    content = <MyConditionElementNoOk />;
+  }
+
   return (
-    <>
-      <h1>{user.name}</h1>
-      <img
-        className="avatar"
-        src={user.imageUrl}
-        alt={'Photo of ' + user.name}
-        style={{
-          width: user.imageSize,
-          height: user.imageSize
-        }}
-      />
-    </>
-  )
+    <div>
+      <Greeting />
+      <hr/><h3>Utilisation de if </h3>
+      {content}
+      <hr/><h3>Utilisation de l'opérateur ternaire </h3>
+      {user ? <MyContent user={user} /> : <MyConditionElementNoOk />}
+      <hr/> <h3> Utilisation de l'opérateur && </h3>
+      {user && <MyContent user={user} />} 
+    </div>
+  );
 }
 
-export default App
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+export default App;
