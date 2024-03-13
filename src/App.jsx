49068da41bebe4d0f67996ac12b0867199ab84f0
b2oba151 <bodiarra20@gmail.com>
2024-03-13 15:20:38 +0000

    liste de produits

--- src/App.jsx
+++ src/App.jsx
@@ -3,69 +3,70 @@ import reactLogo from './assets/react.svg';
 import viteLogo from '/vite.svg'; 
 import './App.css';
 
-/* if
-function Greeting() {
-  const currentHour = new Date().getHours();
-  let greetingMessage;
+/* if */
+// function Greeting() {
+//   const currentHour = new Date().getHours();
+//   let greetingMessage;
 
-  if (currentHour >= 6 && currentHour < 12) {
-    greetingMessage = 'Bonjour';
-  } else if (currentHour >= 12 && currentHour < 18) {
-    greetingMessage = 'Bonne après-midi';
-  } else {
-    greetingMessage = 'Bonne soirée';
-  }
+//   if (currentHour >= 6 && currentHour < 12) {
+//     greetingMessage = 'Bonjour';
+//   } else if (currentHour >= 12 && currentHour < 18) {
+//     greetingMessage = 'Bonne après-midi';
+//   } else {
+//     greetingMessage = 'Bonne soirée';
+//   }
 
-  return (
-    <h2>{greetingMessage}, bienvenue sur notre site !</h2>
-  );
-}
+//   return (
+//     <h2>{greetingMessage}, bienvenue sur notre site !</h2>
+//   );
+// }
 
-function MyContent({ user }) { // Utilisation de la destructuration pour extraire user
-  return (
-    <div>
-      <h1>{user.name}</h1>
-      <img
-        className="avatar"
-        src={user.imageUrl}
-        alt={`Photo de ${user.name}`} // Utilisation de l'interpolation de chaîne
-        style={{
-          width: user.imageSize,
-          height: user.imageSize,
-        }}
-      />
-    </div>
-  );
-}
+// function MyContent({ user }) { // Utilisation de la destructuration pour extraire user
+//   return (
+//     <div>
+//       <h1>{user.name}</h1>
+//       <img
+//         className="avatar"
+//         src={user.imageUrl}
+//         alt={`Photo de ${user.name}`} // Utilisation de l'interpolation de chaîne
+//         style={{
+//           width: user.imageSize,
+//           height: user.imageSize,
+//         }}
+//       />
+//     </div>
+//   );
+// }
 
-function MyConditionElementNoOk() {
-  return (
-    <h3>Condition non vérifiée</h3>
-  );
-}
+// function MyConditionElementNoOk() {
+//   return (
+//     <h3>Condition non vérifiée</h3>
+//   );
+// }
 
-function App() {
-  const user = {
-    name: 'Hedy Lamarr',
-    imageUrl: 'https://i.imgur.com/yXOvdOSs.jpg',
-    imageSize: 100,
-  };
+// function App() {
+//   const user = {
+//     name: 'Hedy Lamarr',
+//     imageUrl: 'https://i.imgur.com/yXOvdOSs.jpg',
+//     imageSize: 100,
+//   };
 
 
-  return (
-    <div>
-      <Greeting />
-      {user ? <MyContent user={user} /> : <MyConditionElementNoOk />}
-    </div>
-  );
-}
-*/
-
+//   return (
+//     <div>
+//       <Greeting />
+//       {user ? <MyContent user={user} /> : <MyConditionElementNoOk />}
+//     </div>
+//   );
+// }
 
 
 
 
-/* liste de produits
+
+
+/* liste de produits*/
+
 const products = [
   { title: 'Choux', isFruit: false, id: 1 },
   { title: 'Ail', isFruit: false, id: 2 },
@@ -99,7 +100,7 @@ function App() {
     </>
   );
 }
-*/
+
 
 
 
