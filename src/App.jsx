e51bb420d464577608ed0386248aa30dfbdd85b1
b2oba151 <bodiarra20@gmail.com>
2024-03-13 15:07:29 +0000

    listes_de_rendu

--- src/App.jsx
+++ src/App.jsx
@@ -3,70 +3,26 @@ import reactLogo from './assets/react.svg';
 import viteLogo from '/vite.svg'; 
 import './App.css';
 
-function Greeting() {
-  const currentHour = new Date().getHours();
-  let greetingMessage;
+const products = [
+  { title: 'Choux', id: 1 },
+  { title: 'Ail', id: 2 },
+  { title: 'Pomme', id: 3 },
+  { title: 'Orange', id: 4 },
+];
 
-  if (currentHour >= 6 && currentHour < 12) {
-    greetingMessage = 'Bonjour';
-  } else if (currentHour >= 12 && currentHour < 18) {
-    greetingMessage = 'Bonne après-midi';
-  } else {
-    greetingMessage = 'Bonne soirée';
-  }
 
-  return (
-    <h2>{greetingMessage}, bienvenue sur notre site !</h2>
+const listItems = products.map(product =>
+  //key pour permettre le CRUD facilement apres
+  <li key={product.id}> {product.title}</li> 
   );
-}
-
-function MyContent({ user }) { // Utilisation de la destructuration pour extraire user
-  return (
-    <div>
-      <h1>{user.name}</h1>
-      <img
-        className="avatar"
-        src={user.imageUrl}
-        alt={`Photo de ${user.name}`} // Utilisation de l'interpolation de chaîne
-        style={{
-          width: user.imageSize,
-          height: user.imageSize,
-        }}
-      />
-    </div>
-  );
-}
-
-function MyConditionElementNoOk() {
-  return (
-    <h3>Condition non vérifiée</h3>
-  );
-}
 
 function App() {
-  const user = {
-    name: 'Hedy Lamarr',
-    imageUrl: 'https://i.imgur.com/yXOvdOSs.jpg',
-    imageSize: 100,
-  };
-
-
   return (
-    <div>
-      <Greeting />
-      {user ? <MyContent user={user} /> : <MyConditionElementNoOk />}
-    </div>
+    <>
+    <ul>{listItems}</ul>
+    </>
   );
 }
-
-
-
-
-
-
-
-
-
 
 
 
